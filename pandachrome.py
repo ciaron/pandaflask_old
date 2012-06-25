06c612b96d5509567fd0321888a8dd96acda87d5
Ciaron Linstead <ciaron.linstead@gmail.com>
2012-06-25 21:55:41 +0200

    moved to flask-uploads

--- pandachrome.py
+++ pandachrome.py
@@ -1,7 +1,10 @@
 import os
+from datetime import datetime
 from flask import Flask, request, redirect, url_for, render_template, session, abort, flash, g
 from flask import send_from_directory
+from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import secure_filename
+from werkzeug import generate_password_hash, check_password_hash
 
 USERNAME="ciaron"
 PASSWORD="default"
@@ -11,8 +14,42 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    pwdhash = db.Column(db.String())
+    email = db.Column(db.String(120), unique=True)
+    activate = db.Column(db.Boolean)
+    created = db.Column(db.DateTime)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.pwdhash = generate_password_hash(password)
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def check_password(self, password):
+        return check_password_hash(self.pwdhash, password)
+
+## Standard Forms
+#class signup_form(Form):
+#    username = TextField('Username', [validators.Required()])
+#    password = PasswordField('Password', [validators.Required(), validators.EqualTo('confirm', message='Passwords must match')])
+#    confirm = PasswordField('Confirm Password', [validators.Required()])
+#    email = TextField('eMail', [validators.Required()])
+#    accept_tos = BooleanField('I accept the TOS', [validators.Required])
+#
+#class login_form(Form):
+#    username = TextField('Username', [validators.Required()])
+#    password = TextField('Password', [validators.Required()])
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -37,10 +74,13 @@ def uploaded_file(filename):
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
+        user = User.query.filter_by(username=request.form['username']).first()
+        if not user.check_password(request.form['password']):
+            error = "Login failed"
+#        if request.form['username'] != app.config['USERNAME']:
+#            error = 'Invalid username'
+#        elif request.form['password'] != app.config['PASSWORD']:
+#            error = 'Invalid password'
 
         else:
             session['logged_in'] = True
